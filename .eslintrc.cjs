df24bc76abbb24cce7e0908b3d027705ffb63403
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2023-11-29 10:41:49 +0800

    build: 创建项目基础配置

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  // 全局变量配置
+  globals: {
+    // 可以添加项目中使用的全局变量，避免ESLint报错
+    globalVar: 'readonly',
+  },
+  // 环境配置
+  env: {
+    browser: true, // 浏览器环境
+    node: true, // Node.js环境
+    es6: true, // 启用ES6全局变量
+  },
+  // 文件过滤器
+  ignorePatterns: ['node_modules/', 'dist/'], // 忽略指定的文件或目录
+}
