85f454d53a17d02e38904ade8b0bc147cbfb8556
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2023-12-12 21:10:12 +0800

    build: 设置vite按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig  } from 'vite'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
@@ -7,7 +12,26 @@ export default () => {
   return defineConfig({
     plugins: [
       vue(),
-      vueJsx()
+      vueJsx(),
+      AutoImport({
+        resolvers: [
+          VantResolver(),
+          IconsResolver({
+            prefix: 'Icon'
+          })
+        ]
+      }),
+      Components({
+        resolvers: [
+          VantResolver(),
+          IconsResolver({
+            enabledCollections: ['ep']
+          })
+        ]
+      }),
+      Icons({
+        autoInstall: true
+      })
     ],
     resolve: {
       alias: {
@@ -19,5 +43,5 @@ export default () => {
       port: 3001,
       open: false
     }
-  });
+  })
 };
